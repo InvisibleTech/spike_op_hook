ee482a957689a0899fc44b7ec67585d6b58589e8
John Ferguson <john.ferguson@kognition.ai>
2020-11-02 13:57:52 -0500

    Fixes and added some default port stuff

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
-
 const app = express();
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
 
-app.post("dump_req_allow", (req, res) => {
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req")
-    console.log(req.body)
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req")
+//app.use(express.static('public'));
+app.use(express.json());
 
-    res.end() // Empty SUCCESS
+app.post("/dump_req_allow", (req, res) => {
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req");
+  console.log(req.body);
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req");
+
+  res.end(); // Empty SUCCESS
+});
+
+const listener = app.listen(process.env.PORT || 0, () => {
+  console.log(`KOP Hook is listening on port ${listener.address().port}`);
 });

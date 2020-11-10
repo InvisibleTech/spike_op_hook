f66d4a3ef53a0925ffeeedfc46becb2d2a09c451
John Ferguson <john.ferguson@kognition.ai>
2020-11-10 16:04:19 -0500

    Add logo file and serve it.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(express.json());
 
 app.post("/dump_req_allow", (req, res) => {

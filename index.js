de1368e718aca44b9bd1ad45da0d9d94d3c4c529
John Ferguson <john.ferguson@kognition.ai>
2020-11-11 09:31:49 -0500

    Fake Auth using API key from Zapier.

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.json());
 
+app.get("/fake_auth", (req, res) => {
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req");
+  console.log(req);
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req");
+
+  res.end(); // Empty SUCCESS
+})
+
 app.post("/dump_req_allow", (req, res) => {
-  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req");
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req body");
   console.log(req.body);
-  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req");
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req body");
 
   res.end(); // Empty SUCCESS
 });

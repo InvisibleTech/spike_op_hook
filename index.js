accb32290d423138613461a406b307a8863fb43e
John Ferguson <john.ferguson@kognition.ai>
2020-11-26 11:32:30 -0500

    Add more dumping

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/fake_auth", (req, res) => {
   console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req");
   console.log(req);
   console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req");
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump query params");
+  console.log(req.query);
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump query params");
 
   res.end(); // Empty SUCCESS
-})
+});
 
 app.post("/dump_req_allow", (req, res) => {
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req");
+  console.log(req);
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req");
   console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req body");
   console.log(req.body);
   console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> dump req body");
